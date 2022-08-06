06b8f38b47d12439135b37e6bbc60bd814baa0c8
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2022-08-06 15:40:02 +0800

    Create fix-wifi-ping-20220806-1537.bat

---
+++ fix-wifi-ping-20220806-1537.bat
@@ -0,0 +1,5 @@
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+
+net stop "WLAN AutoConfig"
+
+net start "WLAN AutoConfig"
